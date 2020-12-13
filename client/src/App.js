c0ec4dc8c7aa5899744cc9e31caab3031f7d2d22
Alan Chen <alanchen@Alans-MBP.fios-router.home>
2020-12-13 14:51:13 -0500

    test config

--- client/src/App.js
+++ client/src/App.js
@@ -16,16 +16,6 @@ import NoMatch from "./pages/NoMatch.js";
 
 import axios from 'axios';
 
-import sslRedirect from 'heroku-ssl-redirect';
-import express from 'express';
-const app = express();
-
-app.use(sslRedirect())
-app.get('/',(req, res) => {
-  res.send('hello world');
-})
-app.listen(process.env.PORT || 3000);
-
 class App extends Component {
   constructor() {
     super()
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require("dotenv").config()
-const express = require("express");
+import express from 'express'
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -20,6 +20,15 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// import sslRedirect from 'heroku-ssl-redirect';
+// import express from 'express';
+// const app = express();
+
+// app.use(sslRedirect())
+// app.get('/',(req, res) => {
+//   res.send('hello world');
+// })
+// app.listen(process.env.PORT || 3000);
 
 
 
